fc91d79d24ba0a7e0352b7dd909f7a06b15450b2
Cezary Rochna <10561@student.puzim.edu.pl>
2021-12-06 12:52:37 +0100

    message

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
+using System.IO;
 namespace mpprog_lab_01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+              StreamReader sr = null;
+            try
+            {
+          
+            sr = new StreamReader ("/home/linuxmint/Desktop/doc"); 
+
+            Console.WriteLine(sr.ReadToEnd());
+            }
+                    catch (FileNotFoundException ex)
+                    {
+                    Console.WriteLine($"File Not Found{ex.FileName}"); 
+                    }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                sr?.Close(); 
+            }
         }
     }
 }
